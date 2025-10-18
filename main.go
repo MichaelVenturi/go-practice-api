2bb0a1647cf8d2d67c21548a95674456718d8e96
MichaelVenturi <michaelventuri58@aol.com>
2025-10-18 02:37:53 -0400

    basic gin route

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080") // localhost 8080
+}
+
+func getEvents(context *gin.Context) {
+	fmt.Println("Get request made")
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
 }
