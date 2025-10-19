b7a5a75114a0ccab26e8cd3bc8444fd79ea18238
MichaelVenturi <michaelventuri58@aol.com>
2025-10-19 14:29:15 -0400

    delete event

--- models/event.go
+++ models/event.go
@@ -37,6 +37,35 @@ func (e *Event) Save() error {
 	return err
 }
 
+func (e *Event) Update() error {
+	query := `
+	UPDATE events
+	SET name = ?, description = ?, location = ?, dateTime = ?
+	WHERE id = ?
+	`
+	sqlstmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer sqlstmt.Close()
+	_, err = sqlstmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	return err
+}
+
+func (e *Event) Delete() error {
+	query := `
+	DELETE FROM events WHERE id = ?
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.ID)
+	return err
+}
+
 func GetAllEvents() ([]Event, error) {
 	query := `
 	SELECT * FROM events
